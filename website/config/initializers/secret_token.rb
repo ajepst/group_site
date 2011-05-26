28215cac1f3fbe432555c8a5ca9267520d01134d
Nola Stowe <nola@gamesalad.com>
2011-05-25 20:46:46 -0500

    initial import of rails 3.1 website

---
+++ website/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Website::Application.config.secret_token = '36f674c799b40ab5d91ed84d3dd17264e21fcf6fdd3230f8df4652d69afbe7848559d8fa66d53041705e153e1cc3f2912712773593d3deaa9458b49fcf9e2eb7'
